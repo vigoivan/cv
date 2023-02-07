5a248fc11ba25fe2d8abecd88fd6b449f9210adc
ivan vigo <ivanvigo@hotmail.com>
2023-02-07 12:03:29 -0300

    2do

--- js/js.js
+++ js/js.js
@@ -1,6 +1,4 @@
 function commentBox(){
     document.getElementById("comentario").textContent = document.getElementById("comment").value;
     window.location = "https://api.whatsapp.com/send?phone="+"5491162206574"+"&text="+document.getElementById("comment").value;
-
-
 }
