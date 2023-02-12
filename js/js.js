49c0465295e7e796274ac371c3c7607dfc5b3542
ivan vigo <ivanvigo@hotmail.com>
2023-02-12 19:33:44 -0300

    meta property 8

--- js/js.js
+++ js/js.js
@@ -2,6 +2,3 @@ function commentBox(){
     document.getElementById("comentario").textContent = document.getElementById("comment").value;
     window.location = "https://api.whatsapp.com/send?phone="+"5491162206574"+"&text="+document.getElementById("comment").value;
 }
-function boton_nav(){
-    
-}
